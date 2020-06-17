0b68a690d6f83566de6f379154dc4d378315b6c5
salmavalencia <48778311+salmavalencia@users.noreply.github.com>
2020-06-17 17:49:50 -0400

    cleaner code

--- pointer_types2.c
+++ pointer_types2.c
@@ -6,13 +6,13 @@ int main() {
 	int int_array[5] = {1, 2, 3, 4, 5};   
 	char *char_pointer;   
 	int *int_pointer;   
-	char_pointer = int_array; // The char_pointer and int_pointer now   
-	int_pointer = char_array; // point to incompatible data types.   
-	for(i=0; i < 5; i++) { // Iterate through the int array with the int_pointer.      
+	char_pointer = int_array; 
+	int_pointer = char_array;    
+	for(i=0; i < 5; i++) {      
 		printf("[integer pointer] points to %p, which contains the char '%c'\n", int_pointer, *int_pointer);      
 		int_pointer = int_pointer + 1;   
 	}   
-	for(i=0; i < 5; i++) { // Iterate through the char array with the char_pointer.
+	for(i=0; i < 5; i++) { 
 	        printf("[char pointer] points to %p, which contains the integer %d\n", char_pointer, *char_pointer);      
 		char_pointer = char_pointer + 1;   
 	}
